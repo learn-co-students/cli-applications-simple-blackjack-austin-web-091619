cd340fee5137da1ed216407fea9093b8dd9753d9
Saul Feliz <646634@gmail.com>
2019-09-10 22:04:45 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,11 +58,9 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21 do
-    hit?(card_total)
-    card_total += card_total
-    return card_total
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
-  display_card_total(card_total)
   end_game(card_total)
 end
     
